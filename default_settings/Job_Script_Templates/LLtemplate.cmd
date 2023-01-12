f555e7d36c0399b3d905607c55a2e546ad41269e
Wheeler, Dylan <dylan.wheeler@ukaea.uk>
2023-01-12 14:50:48 +0000

    added job script template folder to default settings

---
+++ default_settings/Job_Script_Templates/LLtemplate.cmd
@@ -0,0 +1,16 @@
+#!/bin/sh 
+
+# @ output = OUT_FILE
+# @ error = ERROR_FILE
+# @ initialdir = INITIAL_DIR
+# @ notify_user = zb0135
+# @ notification = complete
+# @ jobtype = openmpi
+# @ max_processors = 32
+# @ min_processors = 16
+# @ queue
+
+ENV_VARIABLES
+
+COMMAND
+

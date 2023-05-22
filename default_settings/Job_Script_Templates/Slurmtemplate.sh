17a81295d280486d0baed3d9f74e59927480e1a0
sbradnam <steve.bradnam@ukaea.uk>
2023-05-22 14:58:31 +0100

    Added slurm template file

---
+++ default_settings/Job_Script_Templates/Slurmtemplate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SBATCH --job-name="JADE"
+#SBATCH --workdir=INITIAL_DIR
+#SBATCH --output=OUT_FILE
+#SBATCH --error=ERROR_FILE
+#SBATCH --mem-per-cpu=8000
+#SBATCH --time=16-48:00
+#SBATCH --ntasks=MPI_TASKS
+
+CONFIG_SCRIPT
+
+ENV_VARIABLES
+
+COMMAND

c3919fac0653fa145c7dafe03c50695adbbcb996
alexvalentine94 <Alex.Valentine@ukaea.uk>
2023-12-05 16:22:28 +0000

    build docs path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../jade'))
 
-from jade.__version__ import __version__
+from __version__ import __version__
 
 # -- Project information -----------------------------------------------------
 

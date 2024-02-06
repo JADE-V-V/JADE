36a8f63c090ed873ca89d5b2b8c9ab2426a0b188
Davide Laghi <davide.laghi01@gmail.com>
2024-02-06 14:06:29 +0100

    fixed all utilities tests using tmpdir default fixture

--- tests/utilitiesgui_test.py
+++ tests/utilitiesgui_test.py
@@ -18,6 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with JADE.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import sys
 import os
 import shutil
@@ -33,52 +34,58 @@ import shutil
 import pandas as pd
 import pytest
 
-ACTIVATION_FILE = os.path.join(cp, 'TestFiles', 'libmanager',
-                               'Activation libs.xlsx')
-XSDIR_FILE = os.path.join(cp, 'TestFiles', 'libmanager', 'xsdir')
-ISOTOPES_FILE = os.path.join(root, 'jade', 'resources', 'Isotopes.txt')
+ACTIVATION_FILE = os.path.join(cp, "TestFiles", "libmanager", "Activation libs.xlsx")
+XSDIR_FILE = os.path.join(cp, "TestFiles", "libmanager", "xsdir")
+ISOTOPES_FILE = os.path.join(root, "jade", "resources", "Isotopes.txt")
 
 
 class SessionMockup:
 
-    def __init__(self):
+    def __init__(self, uti_path):
         df_rows = [
-                   ['99c', 'sda', '', XSDIR_FILE],
-                   ['98c', 'acsdc', '', XSDIR_FILE],
-                   ['21c', 'adsadsa', '', XSDIR_FILE],
-                   ['31c', 'adsadas', '', XSDIR_FILE],
-                   ['00c', 'sdas', 'yes', XSDIR_FILE],
-                   ['71c', 'sdasxcx', '', XSDIR_FILE]]
+            ["99c", "sda", "", XSDIR_FILE],
+            ["98c", "acsdc", "", XSDIR_FILE],
+            ["21c", "adsadsa", "", XSDIR_FILE],
+            ["31c", "adsadas", "", XSDIR_FILE],
+            ["00c", "sdas", "yes", XSDIR_FILE],
+            ["71c", "sdasxcx", "", XSDIR_FILE],
+        ]
         df_lib = pd.DataFrame(df_rows)
-        df_lib.columns = ['Suffix', 'Name', 'Default', 'MCNP']
-        self.lib_manager = LibManager(df_lib,
-                                      activationfile=ACTIVATION_FILE,
-                                      isotopes_file=ISOTOPES_FILE)
-        self.path_uti = 'D:\\JADE-linux\\Utilities'
+        df_lib.columns = ["Suffix", "Name", "Default", "MCNP"]
+        self.lib_manager = LibManager(
+            df_lib, activationfile=ACTIVATION_FILE, isotopes_file=ISOTOPES_FILE
+        )
+        # Always use fixtures for temporary directories
+        self.path_uti = uti_path
+        self.conf = ConfMockup()
+
+
+class ConfMockup:
+
+    def __init__(self):
+        self.lib = pd.DataFrame(
+            [["00c", "A"], ["31c", "B"]], columns=["Suffix", "name"]
+        ).set_index("Suffix")
 
 
 class TestUtilities:
 
     @pytest.fixture
-    def session(self):
-        return SessionMockup()
+    def session(self, tmpdir):
+        return SessionMockup(tmpdir)
 
     @pytest.fixture
     def inputfile(self):
-        return os.path.join(cp, 'TestFiles', 'utilitiesgui', 'test.i')
+        return os.path.join(cp, "TestFiles", "utilitiesgui", "test.i")
 
-    @pytest.fixture
-    def outpath(self):
-        return os.path.join(cp, 'tmp')
-
-    def test_translate_input(self, session, inputfile, outpath):
+    def test_translate_input(self, session, inputfile, tmpdir):
         """
         the correctness of the translations is already tested in matreader_test
         """
-        lib = '00c'
-        ans = uty.translate_input(session, lib, inputfile,
-                                  outpath=outpath)
-        shutil.rmtree(outpath)
+        lib = "00c"
+        ans = uty.translate_input(
+            session, lib, inputfile, outpath=tmpdir.mkdir("Translate")
+        )
         assert ans
 
     def test_print_libraries(self, session):
@@ -88,18 +95,18 @@ class TestUtilities:
         uty.print_libraries(session.lib_manager)
         assert True
 
-    def test_print_material_info(self, session, inputfile, outpath):
-        uty.print_material_info(session, inputfile,
-                                outpath=outpath)
+    def test_print_material_info(self, session, inputfile, tmpdir):
+        outpath = tmpdir.mkdir("MaterialInfo")
+        uty.print_material_info(session, inputfile, outpath=outpath)
         testfilename = os.path.basename(inputfile)
-        tag = 'materialinfo.xlsx'
-        fileA = os.path.join(outpath, testfilename+'_'+tag)
-        fileB = os.path.join(cp, 'TestFiles', 'utilitiesgui', tag)
+        tag = "materialinfo.xlsx"
+        fileA = os.path.join(outpath, testfilename + "_" + tag)
+        fileB = os.path.join(cp, "TestFiles", "utilitiesgui", tag)
 
         # --- Do some consistency check on the results ---
         # Check on total fraction of materials to be 1
-        elem_df = pd.read_excel(fileA, sheet_name='Sheet2').ffill()
-        tot_frac = elem_df.groupby('Material').sum()['Material Fraction']
+        elem_df = pd.read_excel(fileA, sheet_name="Sheet2").ffill()
+        tot_frac = elem_df.groupby("Material").sum()["Material Fraction"]
         print(tot_frac)
         assert (tot_frac == 1).all()
 
@@ -107,114 +114,118 @@ class TestUtilities:
         excel_equal(fileA, fileB, 2)
         shutil.rmtree(outpath)
 
-    def test_generate_material(self, inputfile, session):
+    def test_generate_material(self, inputfile, session, tmpdir):
         # using atom fraction
         sourcefile = inputfile
-        materials = ['m1', 'M2']
+        materials = ["m1", "M2"]
         percentages = [0.5, 0.5]
-        newlib = '31c'
-        fraction_type = 'atom'
-        outpath = None
-        uty.generate_material(session, sourcefile, materials, percentages,
-                              newlib, fractiontype=fraction_type,
-                              outpath=outpath)
+        newlib = "31c"
+        fraction_type = "atom"
+        outpath = tmpdir.mkdir("GenerateMaterial")
+        uty.generate_material(
+            session,
+            sourcefile,
+            materials,
+            percentages,
+            newlib,
+            fractiontype=fraction_type,
+            outpath=outpath,
+        )
         filename = os.path.basename(inputfile)
-        fileA = os.path.join(cp, 'TestFiles', 'utilitiesgui', 'newmat_atom')
-        fileB = os.path.join(outpath, filename+'_new Material')
+        fileA = os.path.join(cp, "TestFiles", "utilitiesgui", "newmat_atom")
+        fileB = os.path.join(outpath, filename + "_new Material")
         txt_equal(fileA, fileB)
 
         # using mass fraction
-        fraction_type = 'mass'
-        uty.generate_material(session, sourcefile, materials, percentages,
-                              newlib, fractiontype=fraction_type,
-                              outpath=outpath)
-        fileA = os.path.join(cp, 'TestFiles', 'utilitiesgui', 'newmat_mass')
+        fraction_type = "mass"
+        uty.generate_material(
+            session,
+            sourcefile,
+            materials,
+            percentages,
+            newlib,
+            fractiontype=fraction_type,
+            outpath=outpath,
+        )
+        fileA = os.path.join(cp, "TestFiles", "utilitiesgui", "newmat_mass")
         txt_equal(fileA, fileB)
 
-        shutil.rmtree(outpath)
-
-    def test_switch_fractions(self, session, inputfile, outpath):
+    def test_switch_fractions(self, session, inputfile, tmpdir):
 
         # Switches are properly tested in matreader
-        uty.switch_fractions(session, inputfile, 'mass',
-                             outpath=outpath)
-        uty.switch_fractions(session, inputfile, 'atom',
-                             outpath=outpath)
-        shutil.rmtree(outpath)
+        uty.switch_fractions(session, inputfile, "mass", outpath=tmpdir)
+        uty.switch_fractions(session, inputfile, "atom", outpath=tmpdir)
+
         assert True
 
-    def test_change_ACElib_suffix(self, monkeypatch):
-        acefolder = os.path.join(cp, 'TestFiles', 'utilitiesgui', 'ACEchange',
-                                 '99c')
-        newfolder = os.path.join(os.path.dirname(acefolder),
-                                 '99cnew')
+    def test_change_ACElib_suffix(self, monkeypatch, tmpdir):
+        acefolder = os.path.join(cp, "TestFiles", "utilitiesgui", "ACEchange", "99c")
+        newacefolder = os.path.join(tmpdir.mkdir("ACE"), "99c")
+        shutil.copytree(acefolder, newacefolder)
+        newfolder = os.path.join(tmpdir, "ACE", "99cnew")
 
-        responses = iter([str(acefolder), '99c', '98c'])
-        monkeypatch.setattr('builtins.input', lambda msg: next(responses))
+        responses = iter([str(newacefolder), "99c", "98c"])
+        monkeypatch.setattr("builtins.input", lambda msg: next(responses))
         uty.change_ACElib_suffix()
         for file in os.listdir(newfolder):
             filepath = os.path.join(newfolder, file)
-            with open(filepath, 'r') as infile:
+            with open(filepath, "r") as infile:
                 for line in infile:
                     print(line)
-                    print(line.find('.98c'))
-                    if line.find('.98c') != -1:
+                    print(line.find(".98c"))
+                    if line.find(".98c") != -1:
                         assert True
                     else:
                         assert False
                     break
-        shutil.rmtree(newfolder)
 
-    def test_get_reaction_file(self, monkeypatch, session, outpath):
+    def test_get_reaction_file(self, monkeypatch, session, tmpdir):
         # The correctness of the file is already tested in parserD1S
-        filepath = os.path.join(cp, 'TestFiles', 'utilitiesgui', 'd1stest.i')
-        responses = iter([str(filepath), '99c'])
-        monkeypatch.setattr('builtins.input', lambda msg: next(responses))
-        uty.get_reaction_file(session, outpath=outpath)
-        shutil.rmtree(outpath)
+        filepath = os.path.join(cp, "TestFiles", "utilitiesgui", "d1stest.i")
+        responses = iter([str(filepath), "99c"])
+        monkeypatch.setattr("builtins.input", lambda msg: next(responses))
+        uty.get_reaction_file(session, outpath=tmpdir.mkdir("Reaction"))
         assert True
 
     def test_input_with_option(self, monkeypatch):
-        msg = ''
-        options = ['option1', 'option2']
-        inputs = iter(['wrongoption', 'option1'])
-        monkeypatch.setattr('builtins.input', lambda msg: next(inputs))
+        msg = ""
+        options = ["option1", "option2"]
+        inputs = iter(["wrongoption", "option1"])
+        monkeypatch.setattr("builtins.input", lambda msg: next(inputs))
         valid_input = uty.input_with_options(msg, options)
-        assert valid_input == 'option1'
+        assert valid_input == "option1"
 
-    def test_clean_runtpe(self):
-        folders = os.path.join(cp, 'TestFiles', 'utilitiesgui', 'rmv_runtpe')
-        folders_copy = os.path.join(cp, 'TestFiles', 'utilitiesgui',
-                                    'rmv_runtpe_tmp')
+    def test_clean_runtpe(self, tmpdir):
+        folders = os.path.join(cp, "TestFiles", "utilitiesgui", "rmv_runtpe")
+        folders_copy = os.path.join(tmpdir.mkdir("clear"), "rmv_runtpe_tmp")
         # Copy the folders
         shutil.copytree(folders, folders_copy)
-        try:
-            uty.clean_runtpe(folders_copy)
-            # Check files have been removed
-            example = os.path.join(folders_copy, '00c', 'Example')
-            oktavian = os.path.join(folders_copy, '00c', 'Oktavian',
-                                    'Oktavian_Cr')
-            oktavian2 = os.path.join(folders_copy, '00c', 'Oktavian',
-                                     'Oktavian_Al')
-            sphere = os.path.join(folders_copy, '00c', 'Sphere', 'test')
 
-            assert len(os.listdir(example)) == 1
-            assert len(os.listdir(oktavian)) == 3
-            assert len(os.listdir(oktavian2)) == 3  # No deletion
-            assert len(os.listdir(sphere)) == 3
-            assert os.path.exists(os.path.join(example, 'testtestr'))
+        uty.clean_runtpe(folders_copy)
+        # Check files have been removed
+        example = os.path.join(folders_copy, "00c", "Example")
+        oktavian = os.path.join(folders_copy, "00c", "Oktavian", "Oktavian_Cr")
+        oktavian2 = os.path.join(folders_copy, "00c", "Oktavian", "Oktavian_Al")
+        sphere = os.path.join(folders_copy, "00c", "Sphere", "test")
 
-        finally:
-            # Whatever happens, clean the tmp folder
-            shutil.rmtree(folders_copy)
+        assert len(os.listdir(example)) == 1
+        assert len(os.listdir(oktavian)) == 3
+        assert len(os.listdir(oktavian2)) == 3  # No deletion
+        assert len(os.listdir(sphere)) == 3
+        assert os.path.exists(os.path.join(example, "testtestr"))
 
-    def test_print_XS_EXFOR(self, session):
+    def test_print_XS_EXFOR(self, session, monkeypatch):
         """
         the correctness of the translations is already tested in matreader_test
         """
-
+        msg = ""
+        inputs = iter(["1001", "1", "continue", "31c", "continue", "n"])
+        monkeypatch.setattr("builtins.input", lambda msg: next(inputs))
         uty.print_XS_EXFOR(session)
         assert True
+
+        # TODO test also EXFOR if installed
+
 
 def excel_equal(fileA, fileB, n_sheets):
     for i in range(n_sheets):
@@ -224,6 +235,6 @@ def excel_equal(fileA, fileB, n_sheets):
 
 
 def txt_equal(fileA, fileB):
-    with open(fileA, 'r') as infileA, open(fileB, 'r') as infileB:
+    with open(fileA, "r") as infileA, open(fileB, "r") as infileB:
         for lineA, lineB in zip(infileA, infileB):
             assert lineA == lineB

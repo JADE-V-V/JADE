bd45cfb9827d47a9835f862eb5b8b82052ab67a4
Davide Laghi <davide.laghi01@gmail.com>
2024-02-19 19:04:37 +0100

    check active codes for the lib selection

---
+++ jade/constants.py
@@ -0,0 +1,23 @@
+"""
+
+@author: Jade Development Team
+
+Copyright 2021, the JADE Development Team. All rights reserved.
+
+This file is part of JADE.
+
+JADE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JADE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JADE.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+CODES = {"MCNP": "mcnp", "Serpent": "serpent", "OpenMC": "openmc", "d1S": "d1s"}

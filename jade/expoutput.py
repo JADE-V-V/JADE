55989e80f69f22dc5b2c4289a98dc552652f4cae
DAVIDE LAGHI <davide.laghi01@gmail.com>
2024-05-23 09:12:32 +0200

    solve typing error

--- jade/expoutput.py
+++ jade/expoutput.py
@@ -20,6 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with JADE.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import annotations
 import math
 import os
 import re

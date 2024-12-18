14c3da171a56f862a31c0c6b446864f3942c48ba
DAVIDE LAGHI <davide.laghi01@gmail.com>
2024-12-18 16:49:12 +0100

    fix argparse

--- src/jade/__main__.py
+++ src/jade/__main__.py
@@ -5,10 +5,10 @@ from jade.app.app import JadeApp
 
 def main():
     parser = argparse.ArgumentParser(description="JADE V&V")
-    parser.add_argument("--run", help="run benchmarks")
-    parser.add_argument("--raw", help="process raw results")
-    parser.add_argument("--pp", help="perform complete post-process of the results")
-    parser.add_argument("--gui", help="open the run configuration GUI")
+    parser.add_argument("--run", help="run benchmarks", action="store_true")
+    parser.add_argument("--raw", help="process raw results", action="store_true")
+    parser.add_argument("--pp", help="perform complete post-process of the results", action="store_true")
+    parser.add_argument("--gui", help="open the run configuration GUI", action="store_true")
 
     args = parser.parse_args()
 

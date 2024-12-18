a93127296604d4c40007b7f61cf924b63f129eb8
DAVIDE LAGHI <davide.laghi01@gmail.com>
2024-12-18 20:53:11 +0100

    fix utilities parser

--- src/jade/utilities.py
+++ src/jade/utilities.py
@@ -4,7 +4,7 @@ from jade.app.app import JadeApp
 
 
 def main():
-    parser = argparse.ArgumentParser(description="JADE utilities", action="store_true")
+    parser = argparse.ArgumentParser(description="JADE utilities")
     parser.add_argument("-f", "--fetch", help="Fetch the benchmark inputs", action="store_true")
     parser.add_argument("-r", "--restore", help="Restore default configurations", action="store_true")
     parser.add_argument("--runtpe", help="Remove MCNP runtpe files from sim folders", action="store_true")

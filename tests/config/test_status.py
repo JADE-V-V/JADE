783116f353b869f2d4394c80c368b9d84a361e1a
Davide Laghi <davide.laghi01@gmail.com>
2024-12-20 08:52:58 +0100

    fix raw status test

--- tests/config/test_status.py
+++ tests/config/test_status.py
@@ -15,7 +15,7 @@ class TestGlobalStatus:
     def test_init(self):
         status = GlobalStatus(DUMMY_SIMULATIONS, DUMMY_RAW_RESULTS)
         assert len(status.simulations) == 2
-        assert len(status.raw_data) == 1
+        assert len(status.raw_data) == 3
 
     def test_was_simulated(self):
         status = GlobalStatus(DUMMY_SIMULATIONS, DUMMY_RAW_RESULTS)

2f3d4bf57c8fb9afb33849134461006cc20677a9
mcampos16 <Marta.Campos@ext.f4e.europa.eu>
2025-09-22 18:00:05 +0200

    Addition of the excel config file + corrections

--- tests/post/test_excel_processor.py
+++ tests/post/test_excel_processor.py
@@ -293,3 +293,12 @@ class TestExcelProcessor:
         codelibs = [("mcnp", "FENDL 3.2c"), ("mcnp", "FENDL 3.2c")]
         processor = ExcelProcessor(ROOT_RAW, tmpdir, cfg, codelibs)
         processor.process()
+
+    def test_RCR_SS(self, tmpdir):
+        with as_file(
+            files(default_cfg).joinpath("benchmarks_pp/excel/RCR-SS.yaml")
+        ) as file:
+            cfg = ConfigExcelProcessor.from_yaml(file)
+        codelibs = [("exp", "exp"), ("mcnp", "FENDL 3.2c")]
+        processor = ExcelProcessor(ROOT_RAW, tmpdir, cfg, codelibs)
+        processor.process()

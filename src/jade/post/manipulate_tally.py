505036a30c915592dd053976148d9a162ad421cb
mcampos16 <Marta.Campos@ext.f4e.europa.eu>
2025-09-30 12:39:25 +0200

    Addition of CE flux plots to RCR-Fe and RCR-SS.

--- src/jade/post/manipulate_tally.py
+++ src/jade/post/manipulate_tally.py
@@ -263,12 +263,10 @@ def cumulative_sum(
             / tally[column]
         )
     if norm:
-        # Normalize in percentage to the last value (total reaction rate)
+        # Normalize in percentage to the last value (total sum)
         tally[column] = tally[column] / tally[column].iloc[-1] * 100
         if column == "Value":
-            tally["Error"] = np.sqrt(
-                (tally["Error"] ** 2 + tally["Error"].iloc[-1] ** 2)
-            )
+            tally["Error"] = np.sqrt(tally["Error"] ** 2 + tally["Error"].iloc[-1] ** 2)
     return tally
 
 
